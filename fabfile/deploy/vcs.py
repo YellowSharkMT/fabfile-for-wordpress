f3186fab1a31474800e1ead5f3c26d2641084272
YellowSharkMT <bowdowntozappa@yahoo.com>
2013-03-25 14:14:13 -0600

    Added git push functionality to the vcs.Git class.

--- fabfile/deploy/vcs.py
+++ fabfile/deploy/vcs.py
@@ -4,8 +4,25 @@ from fabric.api import *
 
 import fabfile.settings as settings
 
+# Note: this class does NOT change the env.host_string, it's up to you to set that properly before you execute
+# any commands within the Git class. (For now, anyway...)
 class Git:
 	dirs = settings.dirs
+	source = ''
 	
-	def __init__(self):
+	def __init__(self, source):
+		self.source = source
 		pass
+
+	def push(self, destination = 'origin', branch = 'master', options = ''):
+		""" Executes git push from the webroot of the server. (TODO: make this more flexible?) """
+		cmd = 'git push %(destination)s %(branch)s %(options)s' % {
+			'destination':destination,
+			'branch':branch,
+			'options':options,
+		}
+
+		with cd(settings.dirs.get(self.source).get('webroot')):
+			run(cmd)
+
+		print('Completed git push.')
