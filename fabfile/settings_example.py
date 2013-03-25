77e9a257fbe7415120809ddbe7ce0602f1fee57b
YellowSharkMT <bowdowntozappa@yahoo.com>
2013-03-25 14:12:26 -0600

    Updated settings file, added "backup" server.

--- fabfile/settings_example.py
+++ fabfile/settings_example.py
@@ -27,43 +27,56 @@ WP_PREFIX = 'wp'
 # get passed straight into the rsync command. Absolute paths are best, rsync can be funny about how it uses the
 # tilde/~ character in this context.
 
+## !! WARNING: these two values are going to deprecated, in future versions of this software.
 WP_UPLOADS_PROD_LOCATION = 'user@prodhost.com:/path/to/public_html/wordpress/wp-content/uploads/' # Include trailing slash!!
 WP_UPLOADS_LOCAL_LOCATION = '/www/myproject/wordpress/wp-content/uploads' # No trailing slash here
+## End WARNING
 
 # Host connection strings for Fabric. If env.use_ssh_config is set to True, you can use aliases assigned in
 # your .ssh/config file.
 hosts = {
-    'prod':'user@prodhost.com',
-    'local':'localhost'
+	'prod':'user@prodhost.com',
+	'local':'localhost',
+	'backup':'user@backupserver.com', # For offsite backup. See the fabfile.deploy.backup module for more info.
 }
 
 # Database settings for the Prod % Local servers.
 db_settings = {
-    'local':{
-        'host':'localhost',
-        'user':'user',
-        'pass':'password',
-        'db':'database'
-    },
-    'prod':{
+	'local':{
 		'host':'localhost',
 		'user':'user',
 		'pass':'password',
 		'db':'database'
-    }
+	},
+	'prod':{
+		'host':'localhost',
+		'user':'user',
+		'pass':'password',
+		'db':'database'
+	}
 }
 
 ## Archive folder is used for dumping data, and storing downloaded/uploaded files.
+# Note: No trailing slashes, please. Things might not be 100% bullet-proof in that regards.
 dirs = {
-    'local':{
-        'archive': '/www/_archive/myproject',
+	'local':{
+		'archive': '/www/_archive/myproject',
 		'releases': False,
-        'webroot': '/www/myproject'
-    },
-    'prod':{
-        'archive': '/path/to/archive/folder',
-		'releases': '/path/to/releases/folder',
-        'webroot': '/path/to/public_html',
+		'webroot': '/www/myproject',
+	   	'uploads': '/www/myproject/wp-content/uploads',
+	},
+	'prod':{
+		'archive': '/path/to/archive/folder',
+		'releases': '/path/to/releases/folder', # Only needed for deploy.release module. False if not using that.
+		'webroot': '/path/to/public_html',
 		'git_repo': '/path/to/git/repo.git',
-    }
+		'uploads': '/path/to/wordpress/uploads',
+	},
+	'backup':{
+		'archive': '/path/to/archive/folder',
+		'releases': '/path/to/releases/folder', # Not needed (yet) for backup. Optional.
+		'webroot': '/path/to/public_html', 		# Not needed (yet) for backup. Optional.
+		'git_repo': '/path/to/git/repo.git',
+		'uploads': '/path/to/wordpress/uploads',
+	}
 }
