05d7997e3a1b11ca9091e1c2989b098c93b90af1
YellowSharkMT <bowdowntozappa@yahoo.com>
2013-04-08 14:46:24 -0600

    Added some dry-run functionality to the Release class. Here's some example usages, first just the update, then the update with dry run.
        fab update:local,prod # (this pushes the local git repo up to prod, and runs the Release.perform() process)
        fab update:local,prod,dry_run=True # (commands are display, and NOT executed. Output is prefixed with "DRY RUN" messaging)
    
    Also added a confirmation message to the release process, asking if you're sure you want to proceed with unlinking the webroot, re-linking it, etc.

--- fabfile/deploy/release.py
+++ fabfile/deploy/release.py
@@ -1,6 +1,7 @@
 from __future__ import with_statement
 import time, random, string, sys
 from fabric.api import *
+from fabric.contrib.console import confirm
 
 import fabfile.settings as settings
 
@@ -12,14 +13,23 @@ class Release(object):
 	dirs = settings.dirs
 	git = False
 	svn = False
+	dry_run = False
 	release_name = ''
 	
-	def __init__(self, git = False, svn = False):
+	def __init__(self, git = False, svn = False, dry_run = False):
 		self.git = git
 		self.svn = svn
+		self.dry_run = dry_run
 		self.release_name = settings.PROJECT_NAME + '_release_' + str(time.time())
 		
 	def perform(self, source = 'local', destination = 'prod'):
+		if self.dry_run:
+			print(':: -==- DRY RUN MODE ENABLED -==-')
+			print(':: None of the commands will be executed, they will be printed to the display.')
+			print(':: Certain functionality might not be possible in dry-run mode, we\'ll let ') 
+			print(':: you know when that happens though.')
+			print(':: ------------------------------------')
+					
 		self.pre_perform()
 		# Push files from source webroot to destination repo
 		env.host_string = settings.hosts.get(source)
@@ -30,35 +40,49 @@ class Release(object):
 
 		# this is the to-be-updated code, which does indeed work:
 		with cd(settings.dirs.get(source).get('webroot')):
+			print('[DRY RUN: %s] Preparing to push git repo from %s to %s' % (source, source, destination))
 			# Right here, it is absolutely critical that my local git repo has a remote named "prod". 
 			# TODO: be able to specify branches?
-			run('git push %s master' % destination)
+			cmd = 'git push %s master' % destination
+			if self.dry_run:
+				print(('[DRY RUN: %s] ' % source) + cmd)
+			else:
+				run(cmd)
 			
 		# Pull files from destination repo to destination webroot
 		env.host_string = settings.hosts.get(destination)							
 		webroot = settings.dirs.get(destination).get('webroot')
 		releases = settings.dirs.get(destination).get('releases')
 		
-		run('git clone %s %s/%s' % (settings.dirs.get(destination).get('git_repo'), releases, self.release_name))
-		run('ln -s %s/%s %s.new' % (settings.dirs.get(destination).get('releases'), self.release_name, webroot))
-		run('mv %(webroot)s %(webroot)s.old; mv %(webroot)s.new %(webroot)s; rm %(webroot)s.old' % {'webroot': webroot})
+		cmds = [
+			'git clone %s %s/%s' % (settings.dirs.get(destination).get('git_repo'), releases, self.release_name),
+			'ln -s %s/%s %s.new' % (settings.dirs.get(destination).get('releases'), self.release_name, webroot),
+			'mv %(webroot)s %(webroot)s.old; mv %(webroot)s.new %(webroot)s; rm %(webroot)s.old' % {'webroot': webroot},
+		]
+		confirm_msg = 'About to clone the repo on the destination server, and unlink/re-link the webroot. Proceed?'
+		if self.dry_run:
+			confirm_msg = ('[DRY RUN: %s] ' % destination) + confirm_msg
+			
+		if confirm(confirm_msg):
+			for cmd in cmds:
+				if self.dry_run:
+					print(('[DRY RUN: %s] ' % destination ) + cmd)
+				else:
+					run(cmd)
+		else:
+			print('Release cancelled. Exiting...')
+			sys.exit(0)
 			
 		self.post_perform(destination)
 		
 	def pre_perform(self):
+		# Custom stuff would go here, backing up static files, whatever.
+		# if self.dry_run:
+		# else:
 		pass
 		
 	def post_perform(self, location):
 		# Custom stuff would go here, like if you need to symlink some stuff into place, etc.
+		# if self.dry_run:
+		# else:
 		pass
-		
-		
-		
-			
-	
-			
-
-
-
-
-
