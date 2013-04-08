19e7950ef1df85e884767f2487cdb219d6297ced
YellowSharkMT <bowdowntozappa@yahoo.com>
2013-04-08 09:01:52 -0600

    Added stash & unstash functionality to the db module. also, tidied up the dump_db functionality. had some code-duplication going on, got that nailed.

--- fabfile/db.py
+++ fabfile/db.py
@@ -1,11 +1,12 @@
 import time, random, string, sys
 from fabric.api import *
+from fabric.contrib.console import confirm
 
 ## Import custom settings
 from .settings import *
 
 ## Attempt to load the MySQLdb module.
-try:	
+try:
 	import MySQLdb
 	#print("MySQLdb module found.")
 	use_db = True
@@ -24,47 +25,44 @@ def _prod_server():
 	env.host_string = hosts.get('prod')
 
 def _local_server(): # Note: we use _local_server() so there's ZERO confusion with Fabric's local() function.
-	env.host_string = hosts.get('local')	
+	env.host_string = hosts.get('local')
 
-	
+
 #############################################################################
 ## Public functions for the Fab commands.	
 def import_prod_to_local():
 	""" Imports Wordpress database from Prod to Local. """
 	result = execute(fetch_prod_db)
-	
-	insert_database(result.get(hosts.get('prod')), 'local')	
+
+	insert_database(result.get(hosts.get('prod')), 'local')
 	run_db_migration('local')
 
-	
+
 def import_local_to_prod():
 	""" Imports Wordpress database from Local to Prod. """
-	insert_database(push_local_db(), 'prod')	
+	insert_database(push_local_db(), 'prod')
 	run_db_migration('prod')
 
-	
+
 def insert_database(dump_fn, location = 'local'):
 	""" Inserts Wordpress database to specified location (default is local). """
 	env.host_string = hosts.get(location)
 	db = db_settings.get(location)
 	bash_cmd_vars = (db.get('user'), db.get('pass'), db.get('host'), db.get('db'), dump_fn)
-	run('mysql -u %s -p%s -h %s %s < %s' % bash_cmd_vars)	
+	run('mysql -u %s -p%s -h %s %s < %s' % bash_cmd_vars)
 
-	
-@roles('prod')	
+
 def fetch_prod_db():
 	""" Dumps the Prod database, then downloads it to Local. """
-	result = execute(dump_prod_db)
-	dump_remote_fn, dump_fn = result.get(hosts.get('prod'))
-	#dump_remote_fn, dump_fn = dump_prod_db()
-	
+	result = dump_db('prod')
+	dump_remote_fn, dump_fn = result
 	# Set to Local info for the download & import
 	dump_local_fn = ('%s/%s' % (dirs.get('local').get('archive'), dump_fn))
 	_prod_server()
 	get(dump_remote_fn, dump_local_fn)
 	return dump_local_fn
 
-	
+
 def push_local_db():
 	""" Dumps local database (already migrated to prod settings), uploads to Prod """
 	dump_local_fn, dump_fn = dump_local_db_for_deploy()
@@ -75,36 +73,19 @@ def push_local_db():
 	put(dump_local_fn, dump_remote_fn)
 	return dump_remote_fn
 
-	
-# Returns tuple: dump_full_fn, dump_fn (filenames: full, short)
-@roles('prod')
-def dump_prod_db():
-	""" Dumps Prod database to remote archive folder. """
-	db = db_settings.get('prod')
-	dump_fn = db.get('db','unknowndb') + '.prod.' + str(time.time()) + '.sql'
-	dump_full_fn = dirs.get('prod').get('archive', '~/archive') + '/' + dump_fn
 
-	bash_cmd_vars = (db.get('user'), db.get('pass'), db.get('host'), db.get('db'), dump_full_fn)
-	cmd = ('mysqldump -u %s -p%s -h %s %s > %s' % bash_cmd_vars)
-	run(cmd)
-	return (dump_full_fn, dump_fn)
+# Returns tuple: dump_full_fn, dump_fn (filenames: full, short)
+def __dump_prod_db():
+	""" Dumps Prod database to remote archive folder. """
+	return dump_db('prod')
 
 
 # Returns tuple: dump_full_fn, dump_fn (filenames: full, short)	
-def dump_local_db():
+def __dump_local_db():
 	""" Dumps Local database to local archive folder. """
-	_local_server()
+	return dump_db('local')
 
-	db = db_settings.get('local')
-	dump_fn = db.get('db','unknowndb') + '.local.' + str(time.time()) + '.sql'
-	dump_full_fn = dirs.get('local').get('archive', '~/archive') + '/' + dump_fn
 
-	bash_cmd_vars = (db.get('user'), db.get('pass'), db.get('host'), db.get('db'), dump_full_fn)
-	cmd = ('mysqldump -u %s -p%s -h %s %s > %s' % bash_cmd_vars)
-	run(cmd)
-	return (dump_full_fn, dump_fn)
-
-	
 def dump_local_db_for_deploy():
 	""" Wrapper for dump_local_db. Runs migration to prod, dumps, then migrates back to local. """
 	run_db_migration('prod')
@@ -112,20 +93,56 @@ def dump_local_db_for_deploy():
 	run_db_migration('local')
 	return result
 
-	
+
+def dump_db(host, filename = False):
+	# Execute _prod_server() or _local_server()
+	execute(getattr(sys.modules[__name__], '_%s_server' % host))
+
+	db = db_settings.get(host)
+	if not filename:
+		dump_fn = db.get('db','unknowndb') + '.local.' + str(time.time()) + '.sql'
+	else:
+		dump_fn = filename
+
+	dump_full_fn = dirs.get(host).get('archive', '~/archive') + '/' + dump_fn
+
+	bash_cmd_vars = (db.get('user'), db.get('pass'), db.get('host'), db.get('db'), dump_full_fn)
+	cmd = ('mysqldump -u %s -p%s -h %s %s > %s' % bash_cmd_vars)
+	run(cmd)
+	return (dump_full_fn, dump_fn)
+
+
+@task
+def stash(host = 'local'):
+	""" Dumps the database on the specified host, bug gives it a "stash" name, so it can be pulled back in quickly via `unstash`. (:host) """
+	db = db_settings.get(host)
+	filename = '%s.%s.stash.sql' % ( db.get('db','unknowndb'), host)
+	dump_db(host, filename)
+
+@task
+def unstash(host = 'local'):
+	""" Inserts the "stash"-ed database on the specified host. (:host = local)"""
+	db = db_settings.get(host)
+	filename = '%s.%s.stash.sql' % ( db.get('db','unknowndb'), host)
+	dump_full_fn = dirs.get(host).get('archive', '~/archive') + '/' + filename
+	if confirm('Are you sure you wish to insert the stash database to the `%s` host?' % host, False):
+		if confirm('Do you wish to backup the current database before overwriting it?'):
+			dump_db(host) # temporary-ish, this dumps a backup of the existing DB before un-stashing
+		insert_database(dump_full_fn, 'local')
+
 def run_db_migration(dest='local'):
 	""" Executes MySQL commands for migrating Wordpress from one environment to another. Parameter defaults to "local", but any environment defined in the settings.db dictionary (default: local, prod) can be used. """
 	# Check for whether to use MySQLdb or bash. If MySQLdb, set up the connection.
 	db = db_settings.get('local')
-	wp_url = LOCAL_URL if (dest == 'local') else PROD_URL	
+	wp_url = LOCAL_URL if (dest == 'local') else PROD_URL
 	sql = make_update_sql(db, wp_url)
 
 	if not use_db:
 		perform_bash_update(db, sql)
 	else:
 		perform_mysqldb_update(db, sql)
-		
-		
+
+
 def perform_bash_update(db, sql):
 	for query in sql:
 		# Do MySQL via bash
@@ -134,7 +151,7 @@ def perform_bash_update(db, sql):
 		#print(cmd)
 		run(cmd) # again, forcing this command to run only on local server, NOT prod/remote
 
-		
+
 def perform_mysqldb_update(db, sql):
 	# Force this to work on local databases only!
 	db_conn = MySQLdb.connect(db.get('host'), db.get('user'), db.get('pass'), db.get('db'))
@@ -146,10 +163,10 @@ def perform_mysqldb_update(db, sql):
 		except:
 			print(cursor)
 			print('Migration query failed. Exiting...')
-			sys.exit(0)	
+			sys.exit(0)
 	db_conn.close()
 
-			
+
 def make_update_sql(db, wp_url):
 	# Here's the MySQL commands to update Wordpress. At the simplest, `site_url` and `home` need to be updated, in the wp_options
 	# table. However, you could write any SQL you like here, for example a search/replace for the PROD_URL and the LOCAL_URL,
